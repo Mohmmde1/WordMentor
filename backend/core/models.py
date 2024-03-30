2153dbc934eb9899801c454c67c44702bb071aec
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-03-30 11:14:55 +0800

    feat: implemented base model

--- backend/core/models.py
+++ backend/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from softdelete.models import SoftDeleteModel
 
-# Create your models here.
+class BaseModel(SoftDeleteModel):
+    """
+    Abstract base model that includes common attributes
+    such as created_at, updated_at, and deleted_at.
+    """
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
