d5cbe22ec9507ff57109987d2cccf7f39ce781b1
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-04-08 09:58:09 +0800

    feat: enabled the backend to handle image upload

--- backend/settings/views.py
+++ backend/settings/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
 
-from rest_framework import mixins, viewsets
+from rest_framework import mixins, viewsets, status
+from rest_framework.decorators import action
+from rest_framework.parsers import MultiPartParser, FormParser
+from rest_framework.response import Response
 
 from .serializers import ProfileSerializer
 from .models import Profile
@@ -10,3 +13,15 @@ class ProfileViewSet(mixins.RetrieveModelMixin,
                    viewsets.GenericViewSet):
     serializer_class = ProfileSerializer
     queryset = Profile.objects.all()
+    parser_classes = (MultiPartParser, FormParser)
+
+    @action(detail=True, methods=['post'], url_path='upload-image')
+    def upload_image(self, request, pk=None):
+        profile = self.get_object()
+        serializer = self.get_serializer(profile, data=request.data, partial=True)
+        print(request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
