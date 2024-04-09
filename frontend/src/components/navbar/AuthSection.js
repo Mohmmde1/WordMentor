ff23a9cbef0214dfb9c240e561f99f02442c762a
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-04-09 08:57:14 +0800

    style: use profile icon in the dropdown

--- frontend/src/components/navbar/AuthSection.js
+++ frontend/src/components/navbar/AuthSection.js
@@ -1,18 +1,17 @@
 "use client";
-import { useState, useEffect, useId } from "react";
+import { useState, useEffect } from "react";
 import { toast } from "sonner";
 import { Toaster } from "sonner";
 import SignupModal from "@/components/auth/SignupModal";
 import LoginModal from "@/components/auth/LoginModal";
-
 import { checkUser, fetchProfile, logout } from "@/lib/actions";
 
 export default function AuthSection() {
   const [showLoginModal, setShowLoginModal] = useState(false);
   const [showSignupModal, setShowSignupModal] = useState(false);
   const [loggedIn, setLoggedIn] = useState(false);
-  const [showDropdown, setShowDropdown] = useState(false);
   const [username, setUsername] = useState("");
+  const [avatar, setAvatar] = useState("/media/avatars/avatar.jpeg");
 
   useEffect(() => {
     const isLoggedIn = async () => {
@@ -20,15 +19,15 @@ export default function AuthSection() {
       if (userId) {
         const response = await fetchProfile();
         if (response.user.username) setUsername(response.user.username);
+        if (response.avatar_url) setAvatar(response.avatar_url);
+        setLoggedIn(true);
       }
-      if (userId) setLoggedIn(true);
     };
     isLoggedIn();
   }, [loggedIn]);
 
   const openLoginModal = () => {
     setShowLoginModal(true);
-    setShowDropdown(false); // Close the dropdown after selecting an option
   };
 
   const closeLoginModal = () => {
@@ -37,7 +36,6 @@ export default function AuthSection() {
 
   const openSignupModal = () => {
     setShowSignupModal(true);
-    setShowDropdown(false); // Close the dropdown after selecting an option
   };
 
   const closeSignupModal = () => {
@@ -51,81 +49,52 @@ export default function AuthSection() {
     if (loggedIn) toast.success(message);
     else if (message) toast.error(message);
   };
+
   const handleLogout = async () => {
     await logout();
-
-    toast.success("You have logged out successfuly!");
+    toast.success("You have logged out successfully!");
     setLoggedIn(false);
   };
+
   return (
     <>
       <Toaster richColors closeButton position="top-right" />
       <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
-          <button
-            className="navbar-toggler"
-            type="button"
-            data-bs-toggle="collapse"
-            data-bs-target="#navbarNavDarkDropdown"
-            aria-controls="navbarNavDarkDropdown"
-            aria-expanded="false"
-            aria-label="Toggle navigation"
-            onClick={() => setShowDropdown(!showDropdown)}
-          ></button>
+          <div className="d-flex align-items-center">
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavDarkDropdown"
+              aria-controls="navbarNavDarkDropdown"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            ></button>
+          </div>
           <div
-            className={
-              "collapse navbar-collapse" + (showDropdown ? " show" : "")
-            }
+            className="collapse navbar-collapse justify-content-end"
             id="navbarNavDarkDropdown"
           >
-            <ul className="navbar-nav dropdown-menu-lg">
+            <ul className="navbar-nav">
               <li className="nav-item dropdown">
                 <button
-                  className="btn btn-dark btn-lg dropdown-toggle"
+                  className="btn btn-dark dropdown-toggle"
                   data-bs-toggle="dropdown"
                   aria-expanded="false"
                 >
-                  {!useId ? (
-                    <>
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        width="20"
-                        height="20"
-                        fill="currentColor"
-                        className="bi bi-box-arrow-left"
-                        viewBox="0 0 16 16"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0z"
-                        />
-                        <path
-                          fillRule="evenodd"
-                          d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708z"
-                        />
-                      </svg>
-                    </>
+                  {loggedIn ? (
+                    <img
+                      src={process.env.NEXT_PUBLIC_BACKEND_HOST + avatar}
+                      alt="Profile"
+                      className="rounded-circle me-2"
+                      style={{ width: "32px", height: "32px" }}
+                    />
                   ) : (
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      width="20"
-                      height="20"
-                      fill="currentColor"
-                      className="bi bi-box-arrow-in-right"
-                      viewBox="0 0 16 16"
-                    >
-                      <path
-                        fillRule="evenodd"
-                        d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0z"
-                      />
-                      <path
-                        fillRule="evenodd"
-                        d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
-                      />
-                    </svg>
+                    "Guest"
                   )}
                 </button>
-                <ul className="dropdown-menu dropdown-menu-dark">
+                <ul className="dropdown-menu dropdown-menu-dark dropdown-menu-end">
                   {loggedIn ? (
                     <>
                       <li>
@@ -133,14 +102,51 @@ export default function AuthSection() {
                           className="dropdown-item"
                           href={`/profile/${username}`}
                         >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-person-circle"
+                            viewBox="0 0 16 16"
+                            style={{ marginRight: "8px" }}
+                          >
+                            <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0" />
+                            <path
+                              fill-rule="evenodd"
+                              d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8m8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1"
+                            />
+                          </svg>
                           Profile
                         </a>
                       </li>
+                      <div
+                        className="dropdown-divider"
+                        style={{ backgroundColor: "white" }}
+                      ></div>
                       <li>
                         <button
                           className="dropdown-item"
                           onClick={handleLogout}
                         >
+                          <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            width="16"
+                            height="16"
+                            fill="currentColor"
+                            class="bi bi-box-arrow-right"
+                            viewBox="0 0 16 16"
+                            style={{ marginRight: "8px", fill: "red" }}
+                          >
+                            <path
+                              fill-rule="evenodd"
+                              d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0z"
+                            />
+                            <path
+                              fill-rule="evenodd"
+                              d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708z"
+                            />
+                          </svg>
                           Logout
                         </button>
                       </li>
