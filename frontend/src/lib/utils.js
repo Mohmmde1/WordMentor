6a972b7d7c9acc8a55ff56ea714e54a88a9cbc26
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-04-10 12:01:52 +0800

    feat: added verify accesstoken function

--- frontend/src/lib/utils.js
+++ frontend/src/lib/utils.js
@@ -1,4 +1,6 @@
+import apiService from "@/services/apiService";
 import { cookies } from "next/headers";
+import { NextResponse } from "next/server";
 
 // Function to set session cookies
 export default function setSessionCookies(user, access, refresh) {
@@ -33,8 +35,15 @@ export function getAccessToken() {
   return accessToken;
 }
 
+// Function to get access token from cookies
+export function setAccessToken(token) {
+  const response = NextResponse.next();
+  response.cookies.set("session_access_token", token);
+  return response;
+}
+
 // Function to get refresh token from cookies
-export async function getRefreshToken() {
+export function getRefreshToken() {
   const refreshToken = cookies().get("session_refresh_token")?.value;
   return refreshToken;
 }
@@ -44,4 +53,39 @@ export function deleteSessionCookies() {
   cookies().delete("session_userid");
   cookies().delete("session_access_token");
   cookies().delete("session_refresh_token");
+}
+
+export async function updateAccessToken() {
+  const refresh = getRefreshToken();
+  const url = "http://backend:8000/api/v1/auth/token/refresh/";
+  const response = fetch(url, {
+    method: "POST",
+    body: { refresh },
+    headers: {
+      Accept: "*/*",
+      "Content-Type": "*/*",
+    },
+  });
+  console.log(JSON.stringify(response));
+  // const token = response.access;
+  // return setAccessToken(token);
+}
+
+// Function to verify the access token
+export function verifyAccessToken(token) {
+  try {
+    // Decode the token payload
+    const decoded = JSON.parse(
+      Buffer.from(token.split(".")[1], "base64").toString(),
+    );
+
+    // Check if the token has expired
+    const isExpired = Date.now() >= decoded.exp * 1000;
+
+    return !isExpired;
+  } catch (error) {
+    // Handle verification errors
+    console.error("Error verifying access token:", error);
+    return false; // Token is invalid
+  }
 }
