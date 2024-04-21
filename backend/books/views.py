5bf3e3169806366d883ed6063504ee4fee592a2d
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-04-21 11:36:15 +0800

    feat: fetch books using profile-id

--- backend/books/views.py
+++ backend/books/views.py
@@ -1,12 +1,12 @@
 from rest_framework import mixins, viewsets, status
 from rest_framework.response import Response
+from rest_framework.decorators import action
 
 from .models import Book
 from .serializers import BookSerializer
 
 
-class BookViewSet(mixins.ListModelMixin,
-                  mixins.CreateModelMixin,
+class BookViewSet(mixins.CreateModelMixin,
                   mixins.RetrieveModelMixin,
                   mixins.DestroyModelMixin,
                   viewsets.GenericViewSet):
@@ -14,3 +14,11 @@ class BookViewSet(mixins.ListModelMixin,
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     
+    @action(detail=False, methods=['get'], url_path='by-profile/(?P<profile_id>[^/.]+)')  
+    def get_books_by_profile(self, request, profile_id, *args, **kwargs):
+        try:
+            books = Book.objects.filter(profiles__id=profile_id)
+            serializer = BookSerializer(books, many=True)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except:
+            return Response({"error": "Books not found for the given profile ID"}, status=status.HTTP_404_NOT_FOUND)
