5d76c8936e38163c0bab7e6817694b12e34ef366
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-05-07 03:02:12 +0800

    feat: show books link in dropdown

--- frontend/src/components/ProfileDropdown.jsx
+++ frontend/src/components/ProfileDropdown.jsx
@@ -1,7 +1,7 @@
 import { useRouter } from 'next/navigation';
 import Link from 'next/link';
 
-import { LogOut, User, } from 'lucide-react';
+import { LogOut, User, LibraryBig } from 'lucide-react';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
 import { logout } from '@/lib/actions';
 import {
@@ -35,6 +35,14 @@ const ProfileDropdown = ({ profile, setIsAuthenticated }) => {
                 <Link href="/profile">
 
                     <span>Profile</span>
+                </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+
+                <LibraryBig className="mr-2 h-4 w-4" />
+                <Link href="/books">
+
+                    <span>Books</span>
                 </Link>
             </DropdownMenuItem>
             <DropdownMenuSeparator />
