e6f309833a432da0c84dbe4a1e74f9d16856487c
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-05-06 20:27:34 +0800

    bugfix: filter by title not email

--- frontend/src/app/books/DataTable.jsx
+++ frontend/src/app/books/DataTable.jsx
@@ -66,10 +66,10 @@ export function DataTable({
         <div>
             <div className="flex items-center py-4">
                 <Input
-                    placeholder="Filter emails..."
-                    value={(table.getColumn("email")?.getFilterValue()) ?? ""}
+                    placeholder="Filter title..."
+                    value={(table.getColumn("title")?.getFilterValue()) ?? ""}
                     onChange={(event) =>
-                        table.getColumn("email")?.setFilterValue(event.target.value)
+                        table.getColumn("title")?.setFilterValue(event.target.value)
                     }
                     className="max-w-sm"
                 />
