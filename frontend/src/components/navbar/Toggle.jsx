542fd2ae20997b032a0906ec09898b7d55997cfd
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-05-08 13:19:39 +0800

    bugfix: hide focus when click on theme switch

--- frontend/src/components/navbar/Toggle.jsx
+++ frontend/src/components/navbar/Toggle.jsx
@@ -18,9 +18,9 @@ export function ModeToggle() {
   return (
     <DropdownMenu asChild>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon" className="bg-inherit border-none clicked:border-none focus:border-none hover:bg-inherit">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+        <Button variant="outline" size="icon" className="focus:outline-none bg-inherit border-none clicked:border-none focus:border-none hover:bg-inherit">
+          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0 hover:text-white" />
+          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"  />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
