54eb248cc8e496b91546aacab1966158b9c01895
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-05-22 06:19:32 +0800

    chore: update entrypoint

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -7,14 +7,5 @@ set -e
 python manage.py makemigrations
 python manage.py migrate
 
-# Collect static files
-python manage.py collectstatic --noinput
-
-# Check if we need to start the Celery worker
-if [ "$1" = "celery" ]; then
-    shift
-    celery -A backend worker --uid nobody -l info "$@"
-else
-    # Start the Django server
-    exec "$@"
-fi
+# Start the Django server
+python manage.py runserver 0.0.0.0:8000
