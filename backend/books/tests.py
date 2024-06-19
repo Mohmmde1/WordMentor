cbfbe45672767d1d579c9728362ec91b7b519685
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-06-19 22:47:39 +0800

    test: test books model

--- backend/books/tests.py
+++ backend/books/tests.py
@@ -1,3 +1,128 @@
+import re
 from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
+from unittest.mock import patch, MagicMock
+from io import BytesIO
+from rest_framework.exceptions import PermissionDenied
+from django.core.files.uploadedfile import SimpleUploadedFile
+from wordmentor_auth.models import User
+from settings.models import UserProfile
+from books.models import UserBook
+from books.serializers import UserBookSerializer
 
-# Create your tests here.
+
+class BookViewSetTestCase(TestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user = User.objects.create_user(
+            username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = UserProfile.objects.create(user=self.user)
+        self.client.force_authenticate(user=self.user)
+
+    @patch('books.views.PdfReader')
+    def test_create_book_success(self, MockPdfReader):
+        mock_pdf = MagicMock()
+        mock_pdf.pages = [1, 2, 3]
+        MockPdfReader.return_value = mock_pdf
+
+        file = BytesIO(b"%PDF-1.4")
+        file.name = 'test.pdf'
+        data = {'file_path': file,
+                'profile': self.user_profile.id, 'title': file.name}
+        response = self.client.post(reverse(
+            'books-list'), data, format='multipart')
+
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertTrue(UserBook.objects.filter(
+            profile=self.user_profile).exists())
+        self.assertEqual(UserBook.objects.get(
+            profile=self.user_profile).pages, 3)
+
+    def test_create_book_no_file_uploaded(self):
+        response = self.client.post(
+            reverse('books-list'), {}, format='multipart')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+        self.assertEqual(response.data, {"error": "No file uploaded"})
+
+    @patch('books.views.PdfReader')
+    def test_create_book_invalid_pdf(self, MockPdfReader):
+        MockPdfReader.side_effect = Exception("Invalid PDF file")
+        file = BytesIO(b"%PDF-1.4")
+        file.name = 'test.pdf'
+        data = {'file_path': file,
+                'profile': self.user_profile.id, 'title': file.name}
+        response = self.client.post(
+            reverse('books-list'), data, format='multipart')
+
+        self.assertEqual(response.status_code,
+                         status.HTTP_500_INTERNAL_SERVER_ERROR)
+        self.assertEqual(response.data, {"error": "Invalid PDF file"})
+
+    def test_get_books_by_profile_success(self):
+        mock_file = SimpleUploadedFile('test.pdf', b'%PDF-1.4')
+        book = UserBook.objects.create(
+            profile=self.user_profile, pages=10, title='test.pdf', file_path=mock_file)
+
+        response = self.client.get(
+            reverse('books-get-books-by-profile', args=[self.user_profile.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.data), 1)
+        self.assertEqual(response.data[0]['id'], book.id)
+    def test_get_books_by_profile_not_found(self):
+        response = self.client.get(
+            reverse('books-get-books-by-profile', args=[999]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+        self.assertEqual(
+            response.data, {"error": "Profile not found for the given profile ID"})
+
+    @patch('books.views.UserProfile.objects.get')
+    @patch('books.views.UserBook.objects.filter')
+    def test_get_books_by_profile_permission_denied(self, mock_filter, mock_get):
+        mock_get.side_effect = PermissionDenied
+
+        response = self.client.get(
+            reverse('books-get-books-by-profile', args=[self.user_profile.id]))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(
+            response.data, {"error": "You do not have permission to access this profile"})
+
+
+class UserBookModelTest(TestCase):
+
+    def setUp(self):
+        # Create a user and user profile
+        self.user = User.objects.create_user(username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = UserProfile.objects.create(user=self.user)
+
+        # Create a mock file using SimpleUploadedFile
+        self.mock_file = SimpleUploadedFile('test.pdf', b'%PDF-1.4')
+
+    def test_user_book_creation(self):
+        # Create a UserBook instance
+        user_book = UserBook.objects.create(
+            file_path=self.mock_file,
+            pages=10,
+            title='Test Book',
+            profile=self.user_profile
+        )
+
+        # Check that the UserBook instance was created successfully
+        # Use regex to match the file path to handle unique suffix
+        self.assertTrue(re.match(r'books/test.*\.pdf', user_book.file_path.name))
+        self.assertEqual(user_book.pages, 10)
+        self.assertEqual(user_book.title, 'Test Book')
+        self.assertEqual(user_book.profile, self.user_profile)
+
+    def test_user_book_str_representation(self):
+        # Create a UserBook instance
+        user_book = UserBook.objects.create(
+            file_path=self.mock_file,
+            pages=10,
+            title='Test Book',
+            profile=self.user_profile
+        )
+
+        # Check the string representation of the UserBook instance
+        self.assertEqual(str(user_book), 'Test Book')
