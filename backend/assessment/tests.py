94b08a1d0177d987c97ec2d2549064db627dc708
Mohammed Fuad <mohammedfuadalnashriy@gmail.com>
2024-06-19 21:08:37 +0800

    test: test assessment models

--- backend/assessment/tests.py
+++ backend/assessment/tests.py
@@ -11,7 +11,6 @@ from progress_tracking.models import UserWordProgress
 from settings.models import UserProfile
 from .models import UserAssessment, WordAssessment, UserWordAssessmentMapping
 
-
 class AssessmentViewSetTest(TestCase):
     
     def setUp(self):
@@ -99,3 +98,35 @@ class AssessmentViewSetTest(TestCase):
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, {'assessment_exists': False})
+
+class AssessmentModelsTestCase(TestCase):
+    def setUp(self):
+        # Create a user
+        self.user = User.objects.create_user(username='testuser', email='testuser@example.com', password='testpass')
+        self.user_profile = UserProfile.objects.create(user=self.user)
+
+        # Create words
+        self.word1 = Word.objects.create(word='testword1')
+        self.word2 = Word.objects.create(word='testword2')
+
+        # Create WordAssessments
+        self.word_assessment1 = WordAssessment.objects.create(difficulty_level=1, word=self.word1)
+        self.word_assessment2 = WordAssessment.objects.create(difficulty_level=10, word=self.word2)
+
+        # Create UserAssessment
+        self.user_assessment = UserAssessment.objects.create(profile=self.user_profile)
+
+    def test_user_assessment_creation(self):
+        self.assertEqual(str(self.user_assessment), f"Assessment {self.user.username}")
+
+    def test_word_assessment_creation(self):
+        self.assertEqual(str(self.word_assessment1), f"WordAssessment {self.word1.word}")
+        self.assertEqual(str(self.word_assessment2), f"WordAssessment {self.word2.word}")
+
+    def test_user_word_assessment_mapping_creation(self):
+        # Create UserWordAssessmentMapping
+        mapping1 = UserWordAssessmentMapping.objects.create(assessment=self.user_assessment, word_assessment=self.word_assessment1)
+        mapping2 = UserWordAssessmentMapping.objects.create(assessment=self.user_assessment, word_assessment=self.word_assessment2)
+
+        self.assertEqual(str(mapping1), f"UserWordAssessment Assessment {self.user.username} WordAssessment {self.word1.word}")
+        self.assertEqual(str(mapping2), f"UserWordAssessment Assessment {self.user.username} WordAssessment {self.word2.word}")
