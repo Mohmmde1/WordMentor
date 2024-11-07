f249e209c3c516b463f3a803373d72aadd667c76
Mohammed Fuad <mohammed.alnasheri@tigerlab.com>
2024-11-07 16:27:24 +0800

    chore: change static root to static:

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -161,7 +161,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = "static/"
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
