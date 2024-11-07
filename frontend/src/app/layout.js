e10cf238f17878afa1e792af20e37080a2a4636e
Mohammed Fuad <mohammed.alnasheri@tigerlab.com>
2024-11-07 15:33:21 +0800

    chore: collectstatic & fix dynamic issue

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -1,19 +1,20 @@
-import {Inter} from 'next/font/google';
-import {Toaster} from '@/components/ui/sonner';
+export const dynamic = 'force-dynamic'
+import { Inter } from 'next/font/google';
+import { Toaster } from '@/components/ui/sonner';
 
 import './globals.css';
 import MainFooter from '@/app/_components/Footer';
-import {MainNav} from '@/app/_components/navbar/Navbar';
-import {ThemeProvider} from '@/app/_components/ThemeProvider';
+import { MainNav } from '@/app/_components/navbar/Navbar';
+import { ThemeProvider } from '@/app/_components/ThemeProvider';
 
-const inter = Inter ({subsets: ['latin']});
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'Word Mentor',
   description: '',
 };
 
-export default function RootLayout({children}) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
 
@@ -30,7 +31,7 @@ export default function RootLayout({children}) {
             <main className='mb-auto'>{children}</main>
 
             <MainFooter />
-            <Toaster className="top-right"/>
+            <Toaster className="top-right" />
           </div>
         </ThemeProvider>
       </body>
